5f789014624855a6e5c3d7dd20dbd28da4555a45
Twitch1209 <39025598+Twitch1209@users.noreply.github.com>
2018-10-19 14:07:45 +0800

    Update LinkNode&&List.h

--- LinkNode&&List.h
+++ LinkNode&&List.h
@@ -1,29 +1,29 @@
 #include<iostream>
 //#include<string>
+#include<assert.h>
 
 using namespace std;
 
-//enum bool{false,true};
 template<class T>
 class LinearList
 {
 public:
 	LinearList();
 	~LinearList();
-	virtual int Size()const=0;
-	virtual int Length()const=0;
-	virtual int Search(T& x)const=0;
-	virtual int Locate(int i)const=0;
-	virtual bool getData(int i,T& x)const=0;
-	virtual void setData(int i,T& x)=0;
-	virtual bool Insert(int i,T& x)=0;					//insert x after i
-	virtual bool Remove(int i,T& x)=0;					//delete i,return x
-	virtual bool IsEmpty()const =0;
-	virtual bool IsFull()const=0;
-	virtual void Sort()=0;
-	virtual void input()=0;
-	virtual void output()=0;
-	virtual LinearList<T> operator=(LinearList<T>& L)=0;	//copy
+	virtual int Size()const = 0;
+	virtual int Length()const = 0;
+	virtual int Search(T& x)const = 0;
+	//virtual int Locate(int i)=0;
+	virtual bool getData(int i, T& x)const = 0;
+	virtual void setData(int i, T& x) = 0;
+	virtual bool Insert(int i, T& x) = 0;					//insert x after i
+	virtual bool Remove(int i, T& x) = 0;					//delete i,return x
+	virtual bool IsEmpty()const = 0;
+	virtual bool IsFull()const = 0;
+	//virtual void Sort() = 0;
+	//virtual void input() = 0;
+	virtual void output() = 0;
+	virtual LinearList<T>& operator=(LinearList<T>& L) = 0;	//copy
 };
 
 template<class T>
@@ -31,73 +31,75 @@ struct LinkNode
 {
 	T data;
 	LinkNode<T> *link;
-	LinkNode(LinkNode<T> *next=NULL):link(next){}
-	LinkNode(const T& d,LinkNode<T> *next=NULL):data(d),link(next){}
+	LinkNode(LinkNode<T> *next = NULL) :link(next) {}
+	LinkNode(const T& d, LinkNode<T> *next = NULL) :data(d), link(next) {}
 };
 
 template<class T>
-class List: public LinearList<T>
+class List
 {
-	List(){first=new LinkNode<T>;}
-	List(const T& x){first=new LinkNode<T>(x);}
+public:
+	List() { first = new LinkNode<T>; }
+	List(const T& x) { first = new LinkNode<T>(x); }
 	List(List<T>& L);				//copy
-	~List(){makeEmpty();}
+	~List() { makeEmpty(); }
 	void makeEmpty();
 	int Length()const;
-	LinkNode<T> *getHead()const{return first;}			//get head
+	LinkNode<T> *getHead()const { return first; }			//get head
 	LinkNode<T> *Search(T x);
-	LinkNode<T> *Locate(int i);				//search no.i
-	bool getData(int i,T& x)const;
-	void setData(int i,T& x);
-	bool Insert(int i,T& x);
-	bool Remove(int i,T& x);			
-	bool IsEmpty(){return first->link==NULL?true:false;}
-	bool IsFull()const {return false;}
+	LinkNode<T> *Locate(int i)const;				//search no.i
+	bool getData(int i, T& x)const;
+	void setData(int i, T& x);
+	bool Insert(int i, T& x);
+	bool Remove(int i, T& x);
+	bool IsEmpty() { return first->link == NULL ? true : false; }
+	bool IsFull()const { return false; }
 	void sort();
 	void input();
 	void output();
 	List<T>& operator=(List<T>& L);
 private:
-	LinkNode<T> *first,*last;
+	LinkNode<T> *first, *last;
 };
 
 template<class T>
 List<T>::List(List<T>& L)
 {
 	T value;
-	LinkNode<T> *srcptr=L.getHead();
-	LinkNode<T> *destptr=first=new LinkNode<T>;
-	while(srcptr->link!=NULL)
+	LinkNode<T> *srcptr = L.getHead();
+	LinkNode<T> *destptr = first = new LinkNode<T>;
+	while (srcptr->link != NULL)
 	{
-		value=srcptr->link->data;
-		destptr->link=new LinkNode<T>(value);
-		destptr=destptr->link;
-		destptr=srcptr->link;
+		value = srcptr->link->data;
+		destptr->link = new LinkNode<T>(value);
+		destptr = destptr->link;
+		destptr = srcptr->link;
 	}
-	destptr->link=NULL;
+	destptr->link = NULL;
 }
 
 template<class T>
 void List<T>::makeEmpty()
 {
 	LinkNode<T> *q;
-	while(first!=NULL)
+	while (first->link != NULL)
 	{
-		q=first->link;
-		first->link=q->link;
+		q = first->link;
+		first->link = q->link;
 		delete q;
 	}
+	delete first;
 }
 
 template<class T>
 int List<T>::Length()const
 {
-	LinkNode<T> *p=first->link;
-	int count=0;
-	while(p!=NULL)
+	LinkNode<T> *p = first->link;
+	int count = 0;
+	while (p != NULL)
 	{
 		count++;
-		p=p->link;
+		p = p->link;
 	}
 	return count;
 }
@@ -105,73 +107,80 @@ int List<T>::Length()const
 template<class T>
 LinkNode<T> *List<T>::Search(T x)
 {
-	LinkNode<T> *current=first->link;
-	while(current!=NULL)
+	LinkNode<T> *current = first->link;
+	while (current != NULL)
 	{
-		if(current->data==x)
+		if (current->data == x)
 		{
 			break;
 		}
 		else
 		{
-			current=current->link;
+			current = current->link;
 		}
 	}
 	return current;
 }
 
 template<class T>
-LinkNode<T> *List<T>::Locate(int i)
+LinkNode<T>* List<T>::Locate(int i)const
 {
-	if(i<0) return NULL;
-	LinkNode<T> *current=first;
-	int k=0;
-	while(current!=NULL&&k<i)
+	if (i < 0) return NULL;
+	LinkNode<T> *current = first;
+	int k = 1;
+	while (current != NULL && k < i)
 	{
-		current=current->link;
+		current = current->link;
 		k++;
 	}
 	return current;
 }
 
 template<class T>
-bool List<T>::getData(int i,T& x)const
+bool List<T>::getData(int i, T& x)const
 {
-	if(i<=0)return NULL;
-	LinkNode<T>* current=Locate(i);
-	if(current==NULL){return false;}
-	else {x=current->data;return true;}
+	if (i <= 0)return NULL;
+	LinkNode<T>* current = Locate(i);
+	if (current == NULL) { return false; }
+	else { x = current->data; return true; }
 }
 
 template<class T>
-void List<T>::setData(int i,T& x)
+void List<T>::setData(int i, T& x)
 {
-	if(i<=0){return;}
-	LinkNode<T> *current=Locate(i);
-	if(current==NULL) return;
-	else current->data=x;
+	if (i <= 0) { return; }
+	LinkNode<T> *current = Locate(i);
+	if (current == NULL) return;
+	else current->data = x;
 }
 
 template<class T>
-bool List<T>::Insert(int i,T& x)
+bool List<T>::Insert(int i, T& x)
 {
-	LinkNode<T> *current=Locate(i);
-	if(current==NULL) return false;
-	LinkNode<T> *newNode=new LinkNode<T>(x);
-	if(newNode==NULL){ cerr<<"储存分配错误！！！"<<endl;exit(1);}
-	newNode->link=current->link;
-	current->link=newNode;
+	LinkNode<T> *current = Locate(i);
+	LinkNode<T> *newNode = new LinkNode<T>(x);
+	if (newNode == NULL) { cerr << "储存分配错误！！！" << endl; exit(1); }
+	if (i == 1)
+	{
+		first->link = newNode;
+	}
+	else
+	{
+		newNode->link = current->link;
+		current->link = newNode;
+	}
+
 	return true;
 }
 
 template<class T>
-bool List<T>::Remove(int i,T& x)
+bool List<T>::Remove(int i, T& x)
 {
-	LinkNode<T> *current=Locate(i);
-	if(current==NULL||current->link==NULL){return false;}
-	LinkNode<T> *del=current->link;
-	current->link=del->link;
-	x=del->data;
+	LinkNode<T> *current = Locate(i);
+	if (current == NULL || current->link == NULL) { return false; }
+	LinkNode<T> *del = current->link;
+	current->link = del->link;
+	x = del->data;
 	delete del;
 	return true;
 }
@@ -179,11 +188,12 @@ bool List<T>::Remove(int i,T& x)
 template<class T>
 void List<T>::output()
 {
-	LinkNode<T>* current=first->link;
-	while(current!=NULL)
+	LinkNode<T>* current = first->link;
+	if (current == NULL) { cout << "NULL"; }
+	while (current != NULL)
 	{
-		cout<<current->data<<endl;
-		current=current->link;
+		cout << current->data << endl;
+		current = current->link;
 	}
 }
 
@@ -191,24 +201,18 @@ template<class T>
 List<T>& List<T>::operator=(List<T>& L)    //重载赋值操作，使得A=B操作可以实现 
 {
 	T value;
-	LinkNode<T>* srcptr=L.getHead();
-	LinkNode<T>* destptr=first=new LinkNode<T>;
-	while(srcptr->link!=NULL)
+	LinkNode<T>* srcptr = L.getHead();
+	LinkNode<T>* destptr = first = new LinkNode<T>;
+	while (srcptr->link != NULL)
 	{
-		value=srcptr->link->data;
-		destptr->link=new LinkNode<T>(value);
-		destptr=destptr->link;
-		srcptr=srcptr->link;
+		value = srcptr->link->data;
+		destptr->link = new LinkNode<T>(value);
+		destptr = destptr->link;
+		srcptr = srcptr->link;
 	}
-	destptr->link=NULL;
+	destptr->link = NULL;
 	return *this;
 }
-
-
-
-
-
-
 
 
 
