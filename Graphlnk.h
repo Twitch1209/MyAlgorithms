46a9724723cd42840bdc225ed69b3a81b2c1231b
Twitch1209 <39025598+Twitch1209@users.noreply.github.com>
2018-12-04 14:09:56 +0800

    Update Graphlnk.h

--- Graphlnk.h
+++ Graphlnk.h
@@ -36,7 +36,7 @@ public:
 	E getWeight(int v1,int v2);					//get the value of edge(v1,v2)
 	bool insertVertex(const T& v);				//insert vertex v
 	bool removeVertex(int v);					//delete vertex 
-	bool insertEdge(int v1,int v2,E cost);		//insert edge(v1,v2)
+	bool insertEdge(int v1,int v2,E weight);		//insert edge(v1,v2)
 	bool removeEdge(int v1,int v2);				//delete edge(v1,v2)
 	int getFirstNeighbor(int v);				//get v's first neighbor
 	int getNextNeighbor(int v,int w);			//get v->w's next
@@ -44,7 +44,7 @@ private:
 	Vertex<T,E> *NodeTable;						//顶点表（各边链表的头结点）
 	int maxVertices;							//max of Vertices
 	int numEdges;								//the number of edges
-	int numVertives;							//the number of vertices
+	int numVertices;							//the number of vertices
 	int getVertexPos(const T vertex)			//return i from vertex  like:get 0 form a
 	{
 		for(int i=0;i<numVertices;i++)
@@ -184,8 +184,65 @@ bool Graphlnk<T,E>::removeVertex(int v)
 	return true;
 }
 
+//insert (v1,v2)
+bool Graphlnk<T, E>::insertEdge(int v1, int v2, E weight)
+{
+	if (v1 >= 0 && v1 < numVertices && v2 >= 0 && v2 < numVertices)
+	{
+		Edge<T, E> *q, *p = NodeTable[v1].adj;
+		while (p != NULL && p->dest !=v2)
+		{
+			p = p->link;
+		}
+		if (p != NULL){return false;}					// if find this edge, return false
+		//if do not find ,then create new node
+		p = new Edge<T, E>;
+		q = new Edge<T, E>;
+		p->dest = v2;
+		p->cost = weight;
+		p->link = NodeTable[v1].adj;					//link to v1's list
+		NodeTable[v1].adj = p;
+		q->dest = v1;
+		q->cost = weight;
+		q->link = NodeTable[v2].adj;
+		NodeTable[v2].adj = q;
+		numEdges++;
+		return true;
+	}
+}
 
-
+//remove edge(v1,v2)
+bool Graphlnk<T, E>::removeEdge(int v1, int v2)
+{
+	if (v1 != -1 && v2 != -1)
+	{
+		Edge<T, E> *p = NodeTable[v1].adj;
+		Edge<T, E> *q = NULL;
+		Edge<T, E> *s = p;
+		while (p != NULL && p->dest!=v2)							//找被删结点
+		{
+			q = p;
+			p = p->link;
+		}
+		if (p != NULL)
+		{
+			if (p == s){NodeTable[v1].adj = p->link;}				//该结点是边链表的首结点
+			else q->link = p->link;									//不是，重新链接
+			delete p;
+		}
+		else return false;											//没有找到被删边结点
+		//v2对应边链表中删除
+		p = NodeTable[v2].adj;
+		q = NULL;
+		s = p;
+		while (p->dest != v1) { q = p; p = p->link; }
+		if (p == s) NodeTable[v2].adj = p->link;				//该结点是边链表的首结点
+		else q->link = p->link;									//不是，重新链接
+		delete p;
+		return true;
+	}
+	return false;
+}
 
 
 
